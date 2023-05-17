75b45e987780d01c5b08c1ccb5544977f3d59cf6
寒舟 <54969185+Hanzhouu@users.noreply.github.com>
2023-05-17 15:20:56 +0800

    Update Concate Model.py

--- Concate Model.py
+++ Concate Model.py
@@ -14,26 +14,26 @@ from downstream_train import data_load, model_test, num_token
 def concate_layer(model):
     layer_path = './outputs/params/Biology/LayerBert_01/client_0.pt'
 
-    layer_params = torch.load(layer_path)  # 加载客户端的参数
-    params_list = []  # transformer层参数
+    layer_params = torch.load(layer_path)  # loading weights of model on clients
+    params_list = []  # transformer parameters list
 
     for name in layer_params:
         params_list.append(layer_params[name].data)
 
-    for layer in model.bert.encoder.layer[:6]:  # 更改前六层的参数
+    for layer in model.bert.encoder.layer[:6]:  # change 0-5th layers
         for i, p in enumerate(layer.parameters()):
-            p.data = params_list[i]  # 替换transformer层的参数
+            p.data = params_list[i]  # replace transformer
 
     return model
 
 
 def model_cls(model, learning_rate, epochs, device, batch_size):
     """
-    根据拼凑更改的模型进行文本分类任务的训练
-    :param model: 待训练模型
-    :param learning_rate: 学习率
-    :param epochs: 训练轮次
-    :param device: 设备
+    do cls task by concate model
+    :param model: model
+    :param learning_rate: learning_rate
+    :param device: device
+    :param device: device
     :param batch_size: batch_size
     """
     method.setup_seed(40)
@@ -49,16 +49,14 @@ def model_cls(model, learning_rate, epochs, device, batch_size):
     train_dataloader = data_load(token_path, 'train', device, batch_size)
     validation_dataloader = data_load(token_path, 'dev', device, batch_size)
     model.to(device)
-    # 训练开始
+    # start training
 
-    val_acc = []  # 训练过程中的验证集精度
-    # 测试训练结果
-    test_acc = []
+    val_acc = []  # the accuracy of valid
+    test_acc = []  # the accuracy of test
     test_dataloader = data_load(token_path, 'test', device, batch_size)
 
     for i in range(epochs):
         print("Epochs:%d/%d" % ((i + 1), epochs))
-        # 训练开始
         model.train()
         tr_loss = 0
         nb_tr_steps = 0
@@ -66,7 +64,7 @@ def model_cls(model, learning_rate, epochs, device, batch_size):
             batch = tuple(t.to(device) for t in batch)
             input_ids, input_mask, labels = batch
             optimizer.zero_grad()
-            # 取第一个位置，BertForSequenceClassification第一个位置是Loss，第二个位置是[CLS]的logits
+            # first position of BertForSequenceClassification is Loss, second position is logits of [CLS]
             loss = model(input_ids, token_type_ids=None, attention_mask=input_mask, labels=labels)[0]
 
             loss.backward()
@@ -76,7 +74,7 @@ def model_cls(model, learning_rate, epochs, device, batch_size):
             nb_tr_steps += 1
 
         print("Train loss: {}".format(tr_loss / nb_tr_steps))
-        # 模型评估
+        # validate 
         v_acc = model_test(model, validation_dataloader, device, 'Val')
         val_acc.append(v_acc)
         t_acc = model_test(model, test_dataloader, device, 'Test')
@@ -93,11 +91,10 @@ def main():
     model_path = './model/bert-base-uncased/'
 
     modelConfig = BertConfig.from_pretrained(model_path)
-    modelConfig.num_labels = num_token  # 设置分类模型的输出个数
-
+    modelConfig.num_labels = num_token  # set classification numbers
     model = BertForSequenceClassification.from_pretrained(model_path, config=modelConfig)
 
-    # 进行拼凑模型
+    # concate model
     model = concate_layer(model)
 
     val_acc0, test_acc0 = model_cls(model, learning_rate, epochs, device, batch_size)
