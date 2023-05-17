53a6a57fcfa50f0905b0086e432e6d71e4b23675
寒舟 <54969185+Hanzhouu@users.noreply.github.com>
2023-05-17 16:41:23 +0800

    Update downstream_train.py

--- downstream_train.py
+++ downstream_train.py
@@ -27,12 +27,12 @@ import utils
 
 logging.set_verbosity_error()
 
-data_path = './data/text_classification/acl-arc/'  # 数据集路径
-# bert_model = './Bert/bert-base-uncased/'  # 预训练模型的文件
+data_path = './data/text_classification/acl-arc/'  
+# bert_model = './Bert/bert-base-uncased/'  
 
 
-label_num = method.label2num(data_path)  # 获取文件中的句子总共的类别数目
-num_token = len(label_num)  # 分类样本数
+label_num = method.label2num(data_path)  
+num_token = len(label_num)  
 
 
 def setup_seed(seed):
@@ -46,11 +46,6 @@ def setup_seed(seed):
 
 
 def data_read(bert_model, data_type):
-    """
-    将文本分类任务中的数据进行读取预处理
-    :param data_type: 文本的类型：train，test，dev，用于构建地址
-    :return: input_ids, labels, attention_masks
-    """
     sentencses = []
     labels = []
     path = data_path + data_type + '.txt'
@@ -58,9 +53,9 @@ def data_read(bert_model, data_type):
     with open(path, 'r', encoding='utf-8') as file:
         for line in tqdm(file.readlines()):
             dic = json.loads(line)
-            sent = '[CLS] ' + dic['text'] + ' [SEP]'  # 获取句子
-            label_token = dic['label']  # 获取句子标签
-            label = int(label_num[label_token])  # 根据字典将标签转为整型
+            sent = '[CLS] ' + dic['text'] + ' [SEP]'  
+            label_token = dic['label']  
+            label = int(label_num[label_token])  
 
             sentencses.append(sent)
             labels.append(label)
@@ -68,17 +63,12 @@ def data_read(bert_model, data_type):
     tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
     tokenized_sents = [tokenizer.tokenize(sent) for sent in sentencses]
 
-    # 定义句子最大长度（512）
     MAX_LEN = 128
 
-    # 将分割后的句子转化成数字  word-->idx
     input_ids = [tokenizer.convert_tokens_to_ids(sent) for sent in tokenized_sents]
 
-    # 做PADDING,这里使用keras的包做pad
-    # 大于128做截断，小于128做PADDING
     input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
 
-    # 建立mask
     attention_masks = []
     for seq in input_ids:
         seq_mask = [float(i > 0) for i in seq]
@@ -88,27 +78,17 @@ def data_read(bert_model, data_type):
 
 
 def data_load(bert_model, file_type, device, batch_size, split_zie=-0.1):
-    """
-    将Bert初始处理过的数据转换成为Pytorch识别的数据
-    :param bert_model:模型存储地址
-    :param file_type: 文件类型，用于识别应该读取的文件
-    :param device: 是否使用GPU
-    :param batch_size: 批处理的尺寸
-    :param split_zie: 分批出来部分数据集用于训练
-    :return: DataLoader
-    """
+   
     inputs, labels, masks = data_read(bert_model, file_type)  # 获取数据
 
-    # 将数据集转化成tensor
+
     inputs = torch.tensor(inputs).to(device)
     labels = torch.tensor(labels).to(device)
     masks = torch.tensor(masks).to(device)
 
-    # 生成dataloader
     data = TensorDataset(inputs, masks, labels)
 
     if split_zie != -0.1:
-        #  随机划分部分数据集用于训练
         data_size = int(split_zie * len(data))
         print("Choose " + str(split_zie) + " Data for " + file_type + "ing!")
         data, remain_data = random_split(dataset=data, lengths=[data_size, len(data) - data_size])
@@ -120,13 +100,7 @@ def data_load(bert_model, file_type, device, batch_size, split_zie=-0.1):
 
 
 def model_test(model, test_dataloader, device, model_type):
-    """
-    对模型的准确度在特定数据集上进行测试
-    :param model: 待测试的模型
-    :param test_dataloader: 评测数据集
-    :param device: 是否使用GPU
-    :param model_type: 测试模型种类
-    """
+
     model.eval()
     n_true = 0
     n_total = 0
@@ -151,29 +125,14 @@ def model_test(model, test_dataloader, device, model_type):
 
 def train_classier(learning_rate, bert_model, epochs, device, batch_size,
                    hidden_layers=12, change_param=None, share=None, seed=40, drop_layer=3):
-    """
-    在文本分类任务上对模型进行训练
-    :param learning_rate: 学习率
-    :param bert_model:待训练模型
-    :param epochs: 训练轮次
-    :param device: 是否使用GPU
-    :param batch_size: 批处理数据大小
-    :param hidden_layers: Bert中间层的数目
-    :param change_param: 是否重构某些参数
-    :param share: 是否根据已训练参数共享修改其余层参数
-    :param seed: 随机数种子
-    :param drop_layer: 跳过的层
-    """
 
-    # learning_rate = 2e-5  # bigger
-    # 设置随机数种子
     setup_seed(seed)
 
-    param_container = utils.create_container()  # 制作本地参数容器
+    param_container = utils.create_container()  
 
     modelConfig = BertConfig.from_pretrained(bert_model)
-    modelConfig.num_hidden_layers = hidden_layers  # 相当于构建一个小模型，transformer层只有六层
-    modelConfig.num_labels = num_token  # 设置分类模型的输出个数
+    modelConfig.num_hidden_layers = hidden_layers  
+    modelConfig.num_labels = num_token 
 
     model = BertForSequenceClassification.from_pretrained(bert_model, config=modelConfig)
 
@@ -182,11 +141,9 @@ def train_classier(learning_rate, bert_model, epochs, device, batch_size,
                                     drop_layer=drop_layer, ori_layer=drop_layer - 1)
 
     if change_param:
-        # 根据已经保存的文件重新构建模型的某些参数
         model = utils.re_param(model, change_param)
 
     if share:
-        # 根据已经保存某一层参数共享构建其余层的参数
         model = utils.share_param(model, share)
 
     optimizer = torch.optim.AdamW(
@@ -197,10 +154,8 @@ def train_classier(learning_rate, bert_model, epochs, device, batch_size,
     # print(optimizer.param_groups)
 
     model.to(device)
-    # 训练开始
 
-    val_acc = []  # 训练过程中的验证集精度
-    # 测试训练结果
+    val_acc = []  
     test_acc = []
 
     train_dataloader = data_load(bert_model, 'train', device, batch_size)
@@ -213,7 +168,6 @@ def train_classier(learning_rate, bert_model, epochs, device, batch_size,
 
     for i in range(epochs):
         print("Epochs:%d/%d" % ((i + 1), epochs))
-        # 训练开始
         model.train()
         tr_loss = 0
         nb_tr_steps = 0
@@ -221,7 +175,7 @@ def train_classier(learning_rate, bert_model, epochs, device, batch_size,
             batch = tuple(t.to(device) for t in batch)
             input_ids, input_mask, labels = batch
             optimizer.zero_grad()
-            # 取第一个位置，BertForSequenceClassification第一个位置是Loss，第二个位置是[CLS]的logits
+        
             loss = model(input_ids, token_type_ids=None, attention_mask=input_mask, labels=labels)[0]
 
             loss.backward()
@@ -232,7 +186,6 @@ def train_classier(learning_rate, bert_model, epochs, device, batch_size,
 
         scheduler.step()
         print("Train loss: {}".format(tr_loss / nb_tr_steps))
-        # 模型评估
         v_acc = model_test(model, validation_dataloader, device, 'Val')
         val_acc.append(v_acc)
         t_acc = model_test(model, test_dataloader, device, 'Test')
@@ -251,7 +204,7 @@ def down_classier(bert_model, train_dataloader, device, learning_rate, optimizer
         batch = tuple(t.to(device) for t in batch)
         input_ids, input_mask, labels = batch
         optimizer.zero_grad()
-        # 取第一个位置，BertForSequenceClassification第一个位置是Loss，第二个位置是[CLS]的logits
+        
         loss = bert_model(input_ids, token_type_ids=None, attention_mask=input_mask, labels=labels)[0]
 
         loss.backward()
@@ -289,8 +242,8 @@ def find_lr():
         learn_rate = x / (10 ** 6)
         for i in range(6, 0, -1):
             batch_size = 2 ** i
-            print("DownStream Learning Rate:" + str(learn_rate) + "训练中------")
-            print("Batch Size:" + str(batch_size) + "训练中------")
+            print("DownStream Learning Rate:" + str(learn_rate) + "is training------")
+            print("Batch Size:" + str(batch_size) + "is taining------")
             val_acc0, test_acc0 = train_classier(learn_rate, model_path, epochs, device, batch_size,
                                                  change_param=Pro3_6_e5, seed=seed)
 
@@ -306,7 +259,7 @@ def find_lr():
             print("--------------------------------------------------------")
 
     best_index = Max_Acc.index(max(Max_Acc))
-    print("Best 参数为：")
+    print("Best parameters：")
     print(legend[best_index])
     print('Highest Val Acc:%f,Epoch:%d' % (Val_Acc[best_index][Index_list[best_index]], Index_list[best_index]),
           end=', \t')
@@ -376,14 +329,14 @@ def main():
     learn_rate = 5e-5
     device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
-    print("Bert模型训练中------")
-    bert_model0 = './model/bert-base-uncased/'  # 预训练模型的文件
+    print("Bert------")
+    bert_model0 = './model/bert-base-uncased/'  
     val_acc0, test_acc0 = train_classier(learn_rate, bert_model0, epochs, device, batch_size)
 
-    print("LayerBert_03模型训练中------")
+    print("LayerBert_03------")
     val_acc1, test_acc1 = train_classier(learn_rate, bert_model0, epochs, device, batch_size, hidden_layers=3)
 
-    print("LayerBert_06模型训练中------")
+    print("LayerBert_06------")
     val_acc2, test_acc2 = train_classier(learn_rate, bert_model0, epochs, device, batch_size, hidden_layers=6)
 
     legend = ['Bert', 'LayerBert_03', 'LayerBert_06']
