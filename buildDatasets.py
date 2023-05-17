98bf0f4a1835f8899c23693efb85d2b1945a404d
寒舟 <54969185+Hanzhouu@users.noreply.github.com>
2023-05-17 15:10:59 +0800

    Update buildDatasets.py

--- buildDatasets.py
+++ buildDatasets.py
@@ -14,7 +14,7 @@ from transformers import BertTokenizerFast, LineByLineTextDataset
 
 
 def clean_data(in_path, out_path):
-    # 语料json生成txt
+    # json corpus to txt
     file_name = natsort.natsorted(os.listdir(in_path), alg=natsort.ns.PATH)
     for file in file_name:
         file_path = in_path + file
@@ -25,32 +25,31 @@ def save_dataset(bert_model, file_name):
     file_path = './data/sentence/'
     datasets_path = './data/datasets/'
     tokenizer = BertTokenizerFast.from_pretrained(bert_model, do_lower_case=True)
-    print("加载数据中------")
+    print("loading data------")
     T1 = time.time()
-    # 通过LineByLineTextDataset接口 加载数据 #长度设置为128,
+    # loading data and set sentence length to 128,
     train_dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path=file_path + file_name + '.txt', block_size=128)
     T2 = time.time()
-    print('加载数据运行时间:%s 秒' % (T2 - T1))
+    print('Loading data cost: %s second' % (T2 - T1))
 
-    print("保存数据中------")
+    print("Saving data------")
     T3 = time.time()
     torch.save(train_dataset, datasets_path + file_name + '_ori_128.pt')
     T4 = time.time()
-    print('保存数据时间:%s 秒' % (T4 - T3))
+    print('Saving data cost: %s second' % (T4 - T3))
 
 
 def main():
-    print("处理原始语料中------")
-    in_path = './data/S2ORC/Medicine/'
-    out_path = './data/sentence/Medicine.txt'
+    domain = 'Economics'
+    print("Cleaning corpus------")
+    in_path = './data/corpus/'+domain+'/'
+    out_path = './data/sentence/'+domain+'.txt'
     clean_data(in_path, out_path)
-    #
 
-    print("保存数据dataset中------")
+    print("Saving dataset------")
     bert_model = './model/bert-base-uncased/'
-    file_path = 'Medicine'
 
-    save_dataset(bert_model, file_path)
+    save_dataset(bert_model, domain)
 
 
 if __name__ == "__main__":
